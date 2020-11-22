b6a3cdc4a680341e9b80a393fcfeb44f83aa3223
Zhao Yu Lin <>
2020-11-22 16:19:21 -0500

    did gcd()

--- RationalNumber.java
+++ RationalNumber.java
@@ -34,7 +34,12 @@ public class RationalNumber extends RealNumber{
     return numerator + "/" + denominator;
   }
 
-  public static int gcd(int a, int b){
-
+  private static int gcd(int a, int b){
+    while(b!=0){
+      int r = a%b;
+      a = b;
+      b = r;
+    }
+    return a;
   }
 }
