ed72fa581d60863609a5666198a7a504a7e01621
Zhao Yu Lin <>
2020-11-22 13:47:24 -0500

    tested add a bit and it worked

--- RealNumber.java
+++ RealNumber.java
@@ -20,5 +20,9 @@ public class RealNumber{
     double diff = (value-other.getValue())/value;
     return Math.abs(diff)<0.001;
   }
-
+  
+  public RealNumber add(RealNumber other){
+    RealNumber a = new RealNumber(value+other.getValue());
+    return a;
+  }
 }
